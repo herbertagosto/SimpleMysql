2c1dfd983f6b24758fa35a96d5e5c92712417080
Herbert Agosto <herbertagosto@gmail.com>
2021-08-19 15:15:30 +0800

    add SimpleMysql from nuget

--- demo/Program.cs
+++ demo/Program.cs
@@ -17,7 +17,7 @@ namespace demo
             SimpleMysql.Mysql mysql = new SimpleMysql.Mysql("10.34.11.45", "worktable", "skill", "skill");
             var query = "select Premodulename module,refdes from ( select * from worktable.netlist_module a join ( SELECT replace(Predecessor,'_','-') Predecessor, modulename, CONCAT(modulename,'(',replace(Predecessor,'_','-'),')') Premodulename FROM worktable.eedm_placement where pcbno=substring_index('213126','-',1) and predecessor <> 'n/a' and Template = ( select template from worktable.eedm_placement where pcbno=substring_index('213126','-',1) order by createdate desc LIMIT 1) group by Predecessor,modulename,Premodulename ) b on a.no=b.Predecessor and a.module=replace(b.modulename,'_','-') ) c;";
             //query = "SELECT * FROM layoutrecommend.compareresult limit 5000;";
-            //query = "SELECT * FROM worktable.auto_routing  limit 20;";
+            //query = "SELECT * FROM worktable.auto_routing  limit 5000;";
             mysql.Query(query, "D:/temp/test.txt");
             mysql.Dispose();
 
